de06e7df37c8c839eccf75bce6d2f5684d55b981
MateusWMachado <mateusmachado099@gmail.com>
2022-01-30 14:12:49 -0300

    Created initial structure

---
+++ src/main/java/com/mateuswmachado/votesystem/VoteSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mateuswmachado.votesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteSystemApplication.class, args);
+	}
+
+}
