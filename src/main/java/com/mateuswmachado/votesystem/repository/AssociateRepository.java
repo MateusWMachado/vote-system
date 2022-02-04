a99e19d9c69f4b25e2d572a902cbc352f9de4c91
MateusWMachado <mateusmachado099@gmail.com>
2022-02-04 10:38:14 -0300

    Created JPA Repositories

--- src/main/java/com/mateuswmachado/votesystem/repository/AssociateRepository.java
+++ src/main/java/com/mateuswmachado/votesystem/repository/AssociateRepository.java
@@ -4,6 +4,17 @@ import com.mateuswmachado.votesystem.model.Associate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+/** Interface representing an Associate repository
+ *
+ * @author Mateus W. Machado
+ * @since 02/02/2022
+ * @version 1.0.0
+ */
+
 @Repository
 public interface AssociateRepository extends JpaRepository<Associate, Long> {
+
+    Optional<Associate> findByCpf(String cpf);
 }
