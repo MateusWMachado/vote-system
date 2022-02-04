f144c6bf86aadd031a22fcf31f2ab47e17df149b
MateusWMachado <mateusmachado099@gmail.com>
2022-02-04 10:36:15 -0300

    Created API configurations and security

--- src/main/java/com/mateuswmachado/votesystem/configuration/ModelMapperConfig.java
+++ src/main/java/com/mateuswmachado/votesystem/configuration/ModelMapperConfig.java
@@ -4,6 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/** Model mapper configuration class for object conversion
+ *
+ * @author Mateus W. Machado
+ * @since 02/02/2022
+ * @version 1.0.0
+ */
+
 @Configuration
 public class ModelMapperConfig {
 
---
+++ src/main/java/com/mateuswmachado/votesystem/security/SecurityConfigurations.java
@@ -0,0 +1,38 @@
+package com.mateuswmachado.votesystem.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/** API security class where all permissions are open
+ *
+ * @author Mateus W. Machado
+ * @since 02/02/2022
+ * @version 1.0.0
+ */
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable().authorizeRequests()
+                .antMatchers("/**").permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+    }
+
+}
